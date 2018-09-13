2fcb7a7ef2bb2b1856519979e075beadc5730e04
czy95czy <415299462@qq.com>
2018-09-13 18:21:32 +0800

    tour

---
+++ wechat/pages/index/index.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    imgUrls: [
+      './image/1.jpg',
+      './image/2.jpg',
+      './image/3.jpg',
+      './image/4.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  }
+})
